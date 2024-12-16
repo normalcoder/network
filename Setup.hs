91a99c340471610bf5e2bed291b014d31ded55b9
normalcoder <normalcoder@github.com>
2024-12-16 02:23:57 +0100

    call autoreconf -i before build

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
 import Distribution.Simple
+import System.Process (callCommand)
 
 main :: IO ()
-main = defaultMainWithHooks autoconfUserHooks
+main = do
+ defaultMainWithHooks autoconfUserHooks
+  { preConf = \args flags -> do
+        callCommand "echo 'Running autoreconf -i...'"
+        callCommand "autoreconf -i"
+        preConf autoconfUserHooks args flags
+  }

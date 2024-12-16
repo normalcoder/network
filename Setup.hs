21b3ebb151b68988ca82bd75b9f0969fda3a3198
normalcoder <normalcoder@github.com>
2024-12-16 10:43:28 +0100

    Revert "call autoreconf -i before build"
    This reverts commit 91a99c340471610bf5e2bed291b014d31ded55b9.

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,6 @@
 module Main (main) where
 
 import Distribution.Simple
-import System.Process (callCommand)
 
 main :: IO ()
-main = do
- defaultMainWithHooks autoconfUserHooks
-  { preConf = \args flags -> do
-        callCommand "echo 'Running autoreconf -i...'"
-        callCommand "autoreconf -i"
-        preConf autoconfUserHooks args flags
-  }
+main = defaultMainWithHooks autoconfUserHooks
